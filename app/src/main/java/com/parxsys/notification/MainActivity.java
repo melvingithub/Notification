1e13e2b783e9029661abfc55c0719af294d0b58f
melvingithub <melvintcr@gmail.com>
2019-10-18 12:53:20 +0530

    third commit

--- app/src/main/java/com/parxsys/notification/MainActivity.java
+++ app/src/main/java/com/parxsys/notification/MainActivity.java
@@ -16,7 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//make changes
+
+        //created branch in github
 
         Toast.makeText(MainActivity.this, "Welcome!", Toast.LENGTH_SHORT).show();
 
